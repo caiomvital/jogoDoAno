c013d97da68bfddd7a1f5898e081ebb17388b61d
Caio de Matos Vital <81044580+caiomvital@users.noreply.github.com>
2023-08-04 21:54:46 -0300

    Update Dragao.java

--- JogoDoAno/src/model/Dragao.java
+++ JogoDoAno/src/model/Dragao.java
@@ -1,23 +1,74 @@
-package model;
+package model.monsters;
 
-import java.util.concurrent.locks.LockSupport;
+import controller.Calculos;
+import model.Char;
+import model.Monstro;
 
 public abstract class Dragao implements Monstro {
 
-	public void cuspirFogo(int dado, int dano, Char personagem) {
-		dano = 3;
-		if (personagem.getVida() > 0) {
-			if (dado > 3) {
-				System.err.println(this.getNome() + " se prepara para cuspir fogo!");
-				LockSupport.parkNanos(1_000_000_000);
-			}
-			if (dado > 5 && personagem.getDefesa() >= 4) {
-				System.out.println(personagem.getNome() + " escapou do fogo!");
-			} else {
-				System.out.println(personagem.getNome() + " foi atingido pelo fogo!");
-				personagem.setVida(personagem.getVida() - dano);
-				System.err.println("Vida atual de " + personagem.getNome() + ": " + personagem.getVida());
-			} 
-		}
+	private String nome;
+    private int vida;
+    private int ataque;
+
+    public Dragao(int vida, int ataque) {
+    	 this.vida = vida;
+         this.ataque = ataque;
 	}
+
+	public String getNome() {
+        return nome;
+    }
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
+
+    @Override
+    public void receberDano(int dano, Char personagem) {
+        this.vida -= dano;
+    }
+
+    @Override
+    public void causarDano(int dano, Char personagem) {
+        this.ataque = dano;
+        int dado = Calculos.lancarDado();
+        System.out.println("Dado lançado para o ataque de " + this.getNome() + "!!");
+        System.out.println("Resultado do Dado: " + dado);
+        personagem.defender(this);
+        if (dado >= 5) {
+            this.ataqueEspecial(dado, dano, personagem);
+        }
+    }
+
+    @Override
+    public void exibirInformacoes() {
+        System.out.println("Nome do Monstro: " + this.getNome());
+        System.out.println("Pontos de Vida: " + this.getVida());
+        System.out.println("Pontos de Ataque: " + this.getAtaque());
+        if (Calculos.lancarDado() % 2 == 0) {
+            System.out.println(this.getNome() + " está fazendo cara de mau");
+        } else {
+            System.out.println(this.getNome() + " está piscando...");
+        }
+    }
+
+    // Método abstrato para ser implementado nas subclasses de Dragao
+    public abstract void ataqueEspecial(int dado, int dano, Char personagem);
+
+	
 }
