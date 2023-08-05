ef5c228d28c4bf2ab082fab88f4d56f8595d8326
Caio de Matos Vital <81044580+caiomvital@users.noreply.github.com>
2023-08-04 21:57:11 -0300

    Update SlimeVerde.java

--- JogoDoAno/src/view/SlimeVerde.java
+++ JogoDoAno/src/view/SlimeVerde.java
@@ -1,5 +1,46 @@
 package view;
 
-public class SlimeVerde {
+import java.util.concurrent.locks.LockSupport;
+
+import controller.Calculos;
+import model.Char;
+import model.monsters.Slime;
+
+public class SlimeVerde extends Slime{
+
+	public SlimeVerde(int vida, int ataque) {
+		super(vida, ataque);
+		super.setVida(Calculos.lancarDado() * 6);
+		if(super.getVida() < 30) {
+			super.setVida(30);;
+		}
+		super.setAtaque(Calculos.lancarDado() * 4);
+		if(super.getAtaque() < 10) {
+			super.setAtaque(10);
+		}
+		this.setNome("Slime Verde");
+	}
+
+	@Override
+	public void receberDano(int dano, Char personagem) {
+		super.setVida(super.getVida() - dano);
+
+	}
+	public void ataqueEspecial(int dado, int dano, Char personagem) {
+		dano = 2;
+		if (personagem.getVida() > 0) {
+			if (dado >= 4) {
+				System.err.println(this.getNome() + " tentou rastejar por cima de " + personagem.getNome());
+				LockSupport.parkNanos(1_000_000_000);
+			}
+			if (dado <= 3 && personagem.getDefesa() >= 3) {
+				System.out.println(personagem.getNome() + " escapou de " + this.getNome() + "!");
+			} else {
+				System.out.println(this.getNome() + " rastejou por cima de " + personagem.getNome());
+				personagem.setVida(personagem.getVida() - dano);
+				System.err.println("Vida atual de " + personagem.getNome() + ": " + personagem.getVida());
+			} 
+		}
+	}
 
 }
