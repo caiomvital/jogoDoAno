44c8f2a67030d9b9a10e073a1393611bfe68974b
Caio de Matos Vital <81044580+caiomvital@users.noreply.github.com>
2023-08-04 21:55:42 -0300

    Update Slime.java

--- JogoDoAno/src/model/Slime.java
+++ JogoDoAno/src/model/Slime.java
@@ -1,24 +1,76 @@
-package model;
+package model.monsters;
 
-import java.util.concurrent.locks.LockSupport;
+import controller.Calculos;
+import model.Char;
+import model.Monstro;
 
 public abstract class Slime implements Monstro {
 	
-	public void rastejar(int dado, int dano, Char personagem) {
-		dano = 2;
-		if (personagem.getVida() > 0) {
-			if (dado > 4) {
-				System.err.println(this.getNome() + " tentou rastejar por cima de " + personagem.getNome());
-				LockSupport.parkNanos(1_000_000_000);
-			}
-			if (dado > 3 && personagem.getDefesa() >= 3) {
-				System.out.println(personagem.getNome() + " escapou de " + this.getNome() + "!");
-			} else {
-				System.out.println(this.getNome() + " rastejou por cima de " + personagem.getNome());
-				personagem.setVida(personagem.getVida() - dano);
-				System.err.println("Vida atual de " + personagem.getNome() + ": " + personagem.getVida());
-			} 
-		}
+	private String nome;
+    private int vida;
+    private int ataque;
+
+    public Slime(int vida, int ataque) {
+    	 this.vida = vida;
+         this.ataque = ataque;
 	}
+
+	public String getNome() {
+        return nome;
+    }
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
+
+    @Override
+    public void receberDano(int dano, Char personagem) {
+        this.vida -= dano;
+    }
+
+    public void causarDano(int dano, Char personagem) {
+		this.ataque = dano;
+		int dado = Calculos.lancarDado();
+		System.out.println("2)Dado lançado para o ataque de " + this.getNome() + "!!");
+		System.out.println("2)Resultado do Dado: " + dado);
+		personagem.defender(this);
+		if (dado >= 5 && (this.getVida() < this.getVida() / 4)) {
+	        this.ataqueEspecial(dado, dano, personagem);
+	    }
+	}
+
+    public void exibirInformacoes() {
+		System.out.println("Nome do Monstro: " + this.nome);
+		System.out.println("Pontos de Vida: " + this.vida);
+		System.out.println("Pontos de Ataque: " + this.ataque);
+		if(Calculos.lancarDado() % 2 == 0) {
+			System.out.println("O " + this.nome + " está rastejando por aí...");
+		} else {
+			System.out.println("O " + this.nome + " está olhando para o lado...");
+		}
+		
+	}
+
+    // Método abstrato para ser implementado nas subclasses de Dragao
+    public abstract void ataqueEspecial(int dado, int dano, Char personagem);
+
+	
+	
+	
 
 }
